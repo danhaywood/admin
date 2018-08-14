f63ab61efa5dd47e142a7ba7faf11b58791472a3
danhaywood <dan@haywood-associates.co.uk>
2018-08-14 07:35:29 +0200

    updates for monthly reporting process

--- gitlog.sh
+++ gitlog.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
+REPOSROOTS="/c/GITHUB /c/BITBUCKET /c/ASF /c/GITLAB"
+
 today=$(date +"%d %b %Y")
 to_default=$(date -d "$today" +"01 %b %Y")
 from_default=$(date -d "$to_default -1 month" +"%d %b %Y")
-
-DEV_HOME="/c/dev"
-file="$DEV_HOME/_repos.txt"
 
 
 read -p "from? ($from_default): " from
@@ -17,27 +18,32 @@ echo "from: $from" >&2
 echo "to  : $to" >&2
 echo "" >&2
 
-echo "repo,who,yyyy-mm-dd,day,date,descr" > /tmp/$$
+> /tmp/$$
 
-for a in `cat $file | grep -v ^# `
-do  
-	repodir=`echo $a`
+for REPOSROOT in `echo $REPOSROOTS`
+do
+    for a in `ls $REPOSROOT/*/* -1d`
+    do
 
-	pushd $repodir >/dev/null
-    if [ $? -ne 0 ]; then
-        echo "$repodir not cloned..."
-    else
-	    git log --since="$from" --until="$to" --pretty=format:'%cn,%ai,%aD,"%s"' | sed "s/^/${repodir},/" >>/tmp/$$
+		repodir=`echo $a`
 
-	    popd >/dev/null
-	    echo "" >>/tmp/$$
-    fi
+		pushd $repodir >/dev/null
+		if [ $? -ne 0 ]; then
+			echo "$repodir not cloned..."
+		else
+			git log --date="format:%Y-%m-%d %H:%M,%a" --since="$from" --until="$to" --pretty=format:'%cn,%ad,"%s"' | sed "s|^|$repodir,|" | grep -i "haywood" >>/tmp/$$
 
+			popd >/dev/null
+			echo "" >>/tmp/$$
+		fi
+
+	done
 done
 
 
 
-cat /tmp/$$ | sort -k3 | grep -v "^$"
+echo "repo,who,date time,day,descr"
+cat /tmp/$$ | sort -t, -k3 | grep -v "^$" 
 
 rm /tmp/$$
 
